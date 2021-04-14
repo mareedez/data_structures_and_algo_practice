2abcc33a5b73bf31586f98d5fc0030fb77a6de53
mareedez <82243351+mareedez@users.noreply.github.com>
2021-04-13 23:21:55 -0700

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,146 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+  
+  # swapping: O(n) time, O(1) space
+  def swap_nodes(self, input_list, val1, val2):
+    print(f'Swapping {val1} with {val2}')
+
+    node1_prev = None
+    node2_prev = None
+    node1 = input_list.head_node
+    node2 = input_list.head_node
+
+    if val1 == val2:
+      print("Elements are the same - no swap needed")
+      return
+
+    while node1 is not None:
+      if node1.get_value() == val1:
+        break
+      node1_prev = node1
+      node1 = node1.get_next_node()
+
+    while node2 is not None:
+      if node2.get_value() == val2:
+        break
+      node2_prev = node2
+      node2 = node2.get_next_node()
+
+    if (node1 is None or node2 is None):
+      print("Swap not possible - one or more element is not in the list")
+      return
+
+    if node1_prev is None:
+      self.head_node = node2
+    else:
+      node1_prev.set_next_node(node2)
+
+    if node2_prev is None:
+      input_list.head_node = node1
+    else:
+      node2_prev.set_next_node(node1)
+
+    temp = node1.get_next_node()
+    node1.set_next_node(node2.get_next_node())
+    node2.set_next_node(temp)
+  
+  # finding element: O(n) time, O(1) space
+  def nth_last_node(self, linked_list, n):
+    current = None
+    tail_seeker = linked_list.head_node
+    count = 1
+    while tail_seeker:
+        tail_seeker = tail_seeker.get_next_node()
+        count += 1
+        if count >= n + 1:
+            if current is None:
+                current = linked_list.head_node
+            else:
+                current = current.get_next_node()
+    return current
+  
+  # searching for the middle element: O(n) time, O(1) space
+  def find_middle(self, linked_list):
+    fast = linked_list.head_node
+    slow = linked_list.head_node
+
+    while fast:
+        fast = fast.get_next_node()
+        if  fast:
+            fast = fast.get_next_node()
+            slow = slow.get_next_node()
+    return slow
+
+
+def generate_test_linked_list(length):
+  linked_list = LinkedList()
+  for i in range(length, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+
+# example
+ll = LinkedList()
+for i in range(10):
+  ll.insert_beginning(i)
+
+print(ll.stringify_list())
+ll.swap_nodes(ll, 3, 6)
+print(ll.stringify_list())
+
+# finding middle value example
+def generate_test_linked_list(length):
+  for i in range(length, 0, -1):
+    ll.insert_beginning(i)
+  return ll
+
+test_list = generate_test_linked_list(7)
+print(test_list.stringify_list())
+middle_node = ll.find_middle(test_list)
+print(middle_node.value)
